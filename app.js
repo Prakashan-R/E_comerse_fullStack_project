bc1aa7b701d748783bb1c7a2bf8ddc44bd8e51f9
Prakash R Vijay <prakashr714@gmail.com>
2020-10-05 16:10:57 +0530

    create login logout section

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var adminRouter = require('./routes/admin');
 var app = express();
 var fileUpload=require('express-fileupload')
 var db=require('./config/connection')
+var session=require('express-session');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -20,6 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload())
+app.use(session({secret:"key",cookie:{maxAge:600000}}))
 db.connect((err)=>{
   if(err) console.log('Conection Error'+err)
   else console.log('Database Connected to port 27017')
--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,47 @@
+const { response } = require('express');
 var express = require('express');
 var router = express.Router();
 var productHelpers=require('../helpers/product-helpers')
+const userHelpers = require('../helpers/user-helpers')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
+  let user=req.session.user
+  console.log(user)
   productHelpers.getAllProducts().then((products)=>{
-
-    console.log(products)
-
-    res.render('user/view-products',{admin:true,products});
+    res.render('user/view-products',{products,user});
   })
 });
+
+router.get('/login',(req,res)=>{
+  res.render('user/login')
+})
+
+router.get('/signup',(req,res)=>{
+  res.render('user/signup')
+})
+
+
+router.post('/signup',(req,res)=>{
+  userHelpers.doSignup(req.body).then((response)=>{
+    console.log(response)
+  })
+})
+
+router.post('/login',(req,res)=>{
+  userHelpers.doLogin(req.body).then((response)=>{
+    if(response.status){
+      req.session.loggedIn=true
+      req.session.user=response.user
+      res.redirect('/')
+    }else{
+      res.redirect('/login')
+    }
+  })
+})
+
+router.get('/logout',(req,res)=>{
+  req.session.destroy()
+  res.redirect('/')
+})
 
 module.exports = router;
