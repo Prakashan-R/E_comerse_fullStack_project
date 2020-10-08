db72e19b24411321360a3c0a2007a9d21cb24924
Prakash R Vijay <prakashr714@gmail.com>
2020-10-08 16:23:58 +0530

    today cover to edit image add to cart and aggregate

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -2,6 +2,8 @@ var db = require('../config/connection')
 var collection = require('../config/collections');
 const bcrypt = require('bcrypt');
 const { response } = require('express');
+const collections = require('../config/collections');
+var objectId=require('mongodb').ObjectID;
 module.exports={
     doSignup:(userData)=> {
         return new Promise(async(resolve,reject)=>{
@@ -32,6 +34,55 @@ module.exports={
                 console.log("login failed")
                 resolve({status:false})
             }
+        })
+    },
+    addToCart:(prodId,userId)=>{
+        return new Promise(async(resolve,reject)=>{
+            let userCart=await db.get().collection(collections.CART_COLLECTION).findOne({user:objectId(userId)})
+            if(userCart){
+                db.get().collection(collections.CART_COLLECTION)
+                .updateOne({user:objectId(userId)},
+                {
+                    $push:{products:objectId(prodId)}
+                }
+                ).then((response)=>{
+                    resolve()
+                })
+            }else{
+                let cartObj={
+                    user:objectId(userId),
+                    products:[objectId(prodId)]
+                }
+                db.get().collection(collections.CART_COLLECTION).insertOne(cartObj).then((response)=>{
+                    resolve()
+                })
+            }
+        })
+    },
+    getCartProducts:(userId)=>{
+        return new Promise(async(resolve,reject)=>{
+            let cartItems=await db.get().collection(collections.CART_COLLECTION).aggregate([
+                {
+                    $match:{user:objectId(userId)}
+                },
+                {
+                    $lookup:{
+                        from:collection.PRODUCT_COLLECTION,
+                        let:{prodList:'$products'},
+                        pipeline:[
+                            {
+                                $match:{
+                                    $expr:{
+                                        $in:['$_id',"$$prodList"]
+                                    }
+                                }
+                            }
+                        ],
+                        as:'cartItems'
+                    }
+                }
+            ]).toArray()
+            resolve(cartItems[0].cartItems)
         })
     }
 }
--- routes/user.js
+++ routes/user.js
@@ -36,6 +36,9 @@ router.get('/signup',(req,res)=>{
 router.post('/signup',(req,res)=>{
   userHelpers.doSignup(req.body).then((response)=>{
     console.log(response)
+    req.session.loggedIn=true
+    req.session.user=response
+    res.redirect('/')
   })
 })
 
@@ -57,10 +60,14 @@ router.get('/logout',(req,res)=>{
   res.redirect('/')
 })
 
-router.get('/cart',verifyLogin,(req,res)=>{
+router.get('/cart',verifyLogin,async(req,res)=>{
+  let products=await userHelpers.getCartProducts(req.session.user._id)
+  console.log(products)
   res.render("user/cart")
 })
 
-
+router.get('/add-to-cart/:id',verifyLogin,(req,res)=>{
+  userHelpers.addToCart(req.params.id,req.session.user._id)
+})
 
 module.exports = router;
